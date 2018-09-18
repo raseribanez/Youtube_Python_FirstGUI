bdc1b1eb3ea3e49c898194c3690f0c5903a14713
Ben Woodfield <raser.13@hotmail.com>
2018-09-18 17:40:33 +0100

    Add files via upload

---
+++ 01_GUI_py2_basicwindow.py
@@ -0,0 +1,6 @@
+import Tkinter
+from Tkinter import Tk
+
+top = Tk()
+top.minsize(200,200)
+top.mainloop()
---
+++ 01_GUI_py3_basicwindow.py
@@ -0,0 +1,6 @@
+import tkinter
+from tkinter import Tk
+
+top = Tk()
+top.minsize(200,200)
+top.mainloop()
